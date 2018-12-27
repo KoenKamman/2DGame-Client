82a01c79f26b63b6da65f4944655868501ebbbb7
Koen Kamman <kammankoen@gmail.com>
2018-12-27 16:03:54 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,11 @@ export class AppComponent implements OnInit, OnDestroy {
   private pixiContainer: ElementRef;
   private app: PIXI.Application;
 
-  private ships: Ship[] = [];
+  private playerShip: Ship;
+  private ships: Array<Ship>;
 
   constructor() {
+    this.ships = new Array<Ship>();
   }
 
   public ngOnInit(): void {
@@ -33,7 +35,8 @@ export class AppComponent implements OnInit, OnDestroy {
     // Load texture resources
     PIXI.Loader.shared
       .add([
-        "assets/player.png"
+        "assets/player.png",
+        "assets/enemyPlayer.png"
       ])
       .on("progress", this.loadProgressHandler)
       .load(this.setup);
@@ -58,25 +61,27 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   private setup = (): void => {
+    this.spawnPlayer();
+
     let socket = new WebSocket("ws://localhost:8080");
 
+    // Receive messages from server
     socket.onmessage = (event: MessageEvent) => {
-      let data = JSON.parse(event.data);
-      for (let i = 0; i < data.players.length; i++) {
-        let player = data.players[i];
-        let ship = this.ships.find(x => x.id == player.id);
-        if (!ship) {
-          ship = new Ship(PIXI.Loader.shared.resources["assets/player.png"].texture);
-          ship.id = player.id;
-          this.ships.push(ship);
-          this.app.stage.addChild(ship);
-        }
-        ship.position.x = player.x;
-        ship.position.y = player.y;
-        ship.rotation = player.rotation;
+      let message = JSON.parse(event.data);
+      switch(message.type) {
+        case "snapshot":
+          this.renderSnapshot(message.data);
+          break;
+        case "disconnect":
+          this.removePlayer(message.data);
+          break;
+        case "identifier":
+          this.identifierMessageHandler(message.data);
+          break;
       }
     }
 
+    // Send input to server
     let pointerLocation: PIXI.Point;
     this.app.renderer.plugins.interaction.on('pointermove', (event) => {
       pointerLocation = event.data.getLocalPosition(this.app.stage);
@@ -84,6 +89,54 @@ export class AppComponent implements OnInit, OnDestroy {
     setInterval(() => {
       socket.send(JSON.stringify(pointerLocation));
     }, 33)
+  }
+
+  private removePlayer(id: number) {
+    this.app.stage.removeChild(this.ships.find(ship => ship.id == id));
+    this.ships = this.ships.filter(ship => ship.id !== id);
+  }
+
+  private spawnPlayer() {
+    this.playerShip = new Ship(PIXI.Loader.shared.resources["assets/player.png"].texture);
+    this.app.stage.addChild(this.playerShip);
+  }
+
+  private spawnEnemyPlayer(id: number): Ship {
+    let ship = new Ship(PIXI.Loader.shared.resources["assets/enemyPlayer.png"].texture);
+    ship.id = id;
+    this.ships.push(ship);
+    this.app.stage.addChild(ship);
+    return ship;
+  }
+
+  private updatePlayer(ship: Ship, update) {
+    ship.position.x = update.x;
+    ship.position.y = update.y;
+    ship.rotation = update.rotation;
+  }
+
+  private identifierMessageHandler(id: number) {
+    this.playerShip.id = id;
+  }
+
+  private renderSnapshot(snapshot) {
+    for (let i = 0; i < snapshot.players.length; i++) {
+      let player = snapshot.players[i];
+      let ship: Ship;
+
+      if (player.id === this.playerShip.id) {
+        ship = this.playerShip;
+      } else {
+        ship = this.ships.find(x => x.id == player.id);
+      }
+
+      if (!ship) {
+        ship = this.spawnEnemyPlayer(player.id);
+      }
+
+      this.updatePlayer(ship, player);
+
+    }
   }
 
   private lerp(v0: number, v1: number, t: number): number {
