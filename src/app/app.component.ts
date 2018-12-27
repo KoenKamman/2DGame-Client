8b4051239580b2ca49de20629db50a415be88ada
Koen Kamman <kammankoen@gmail.com>
2018-12-27 21:43:32 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,11 @@ export class AppComponent implements OnInit, OnDestroy {
   private playerShip: Ship;
   private ships: Array<Ship>;
 
+  private snapshotBuffer: any[];
+
   constructor() {
     this.ships = new Array<Ship>();
+    this.snapshotBuffer = [];
   }
 
   public ngOnInit(): void {
@@ -49,7 +52,7 @@ export class AppComponent implements OnInit, OnDestroy {
    * Resizes the canvas to fit the parent element
    */
   private resizeHandler = (): void => {
-    const parent = <Element> this.app.view.parentNode;
+    const parent = <Element>this.app.view.parentNode;
     this.app.renderer.resize(parent.clientWidth, parent.clientHeight);
     this.app.stage.position.x = this.app.renderer.width / 2;
     this.app.stage.position.y = this.app.renderer.height / 2;
@@ -65,15 +68,27 @@ export class AppComponent implements OnInit, OnDestroy {
     // Receive messages from server
     socket.onmessage = (event: MessageEvent) => {
       let message = JSON.parse(event.data);
-      switch(message.type) {
+      switch (message.type) {
         case "snapshot":
-          this.renderSnapshot(message.data);
+          if (this.snapshotBuffer.length === 2) {
+            this.snapshotBuffer[0] = this.snapshotBuffer[1];
+            this.snapshotBuffer[1] = message.data;
+          } else {
+            this.snapshotBuffer.push(message.data);
+          }
+          t = 0;
+          break;
+        case "connect":
+          this.spawnEnemyPlayer(message.data);
           break;
         case "disconnect":
           this.removePlayer(message.data);
           break;
-        case "identifier":
-          this.spawnPlayer(message.data);
+        case "server_info":
+          this.spawnPlayer(message.data.playerId);
+          message.data.players.forEach(player => {
+            this.spawnEnemyPlayer(player.id);
+          });
           break;
       }
     }
@@ -86,6 +101,30 @@ export class AppComponent implements OnInit, OnDestroy {
     setInterval(() => {
       socket.send(JSON.stringify(pointerLocation));
     }, 33)
+
+    // Render snapshots
+    let t = 0;
+    PIXI.Ticker.shared.add(() => {
+      t += PIXI.Ticker.shared.elapsedMS;
+      let lag = 1000 / 20;
+      if (this.snapshotBuffer.length === 2) {
+        lag = this.snapshotBuffer[1].timestamp - this.snapshotBuffer[0].timestamp;
+        this.snapshotBuffer[1].players.forEach(player => {
+          let oldPlayer = this.snapshotBuffer[0].players.find(x => x.id == player.id);
+          let ship: Ship;
+          if (player.id == this.playerShip.id) {
+            ship = this.playerShip;
+          } else {
+            ship = this.ships.find(ship => ship.id == player.id);
+          }
+
+          if (!ship || !oldPlayer) return;
+          ship.x = this.lerp(oldPlayer.x, player.x, t / lag);
+          ship.y = this.lerp(oldPlayer.y, player.y, t / lag);
+          ship.rotation = player.rotation;
+        });
+      }
+    });
   }
 
   private removePlayer(id: number) {
@@ -101,45 +140,17 @@ export class AppComponent implements OnInit, OnDestroy {
     Logger.log("SpawnedPlayer", "Spawned player with ID #" + id);
   }
 
-  private spawnEnemyPlayer(id: number): Ship {
+  private spawnEnemyPlayer(id: number) {
     let ship = new Ship(PIXI.Loader.shared.resources["assets/enemyPlayer.png"].texture);
     ship.id = id;
     this.ships.push(ship);
     this.app.stage.addChild(ship);
     Logger.log("SpawnedEnemyPlayer", "Spawned enemy player with ID #" + id);
-    return ship;
-  }
-
-  private updatePlayer(ship: Ship, update) {
-    ship.position.x = update.x;
-    ship.position.y = update.y;
-    ship.rotation = update.rotation;
-  }
-
-  private renderSnapshot(snapshot) {
-    for (let i = 0; i < snapshot.players.length; i++) {
-      let player = snapshot.players[i];
-      let ship: Ship;
-
-      if (player.id === this.playerShip.id) {
-        ship = this.playerShip;
-      } else {
-        ship = this.ships.find(x => x.id == player.id);
-      }
-
-      if (!ship) {
-        ship = this.spawnEnemyPlayer(player.id);
-      }
-
-      this.updatePlayer(ship, player);
-
-    }
   }
 
   private lerp(v0: number, v1: number, t: number): number {
     return (1 - t) * v0 + t * v1;
   }
-
 
   public ngOnDestroy(): void {
 
